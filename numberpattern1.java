32dcc0a0107e6c9b0812ea549e0ce40181583463
Muruganantham M <113437752+MURUGANANTHAM07@users.noreply.github.com>
2024-08-27 13:28:19 +0530

    Create numberpattern1.java

---
+++ numberpattern1.java
@@ -0,0 +1,13 @@
+public class number1 {
+     public static void main(String[]args){
+        java.util.Scanner sc=new java.util.Scanner(System.in);
+        System.out.print("enter n value");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+          for(int j=1;j<=n;j++){
+            System.out.print(i+" ");  
+        }
+          System.out.println();
+    }
+    }
+}
